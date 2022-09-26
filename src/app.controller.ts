eaaa57f18e1c23c638709728de863ca550dd542c
Скоробогатый Евгений <evskorobogatij@gmail.com>
2022-09-26 16:21:27 +0300

    Получение спецификации апи

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,26 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { ApiBody, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  Post,
+  Request,
+  Response,
+  StreamableFile,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBody,
+  ApiExcludeEndpoint,
+  ApiHideProperty,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { createReadStream } from 'fs';
 import { AuthService } from './auth/auth.service';
 import { AuthInfo } from './auth/dto/auth-info.dto';
 import { AuthResult } from './auth/dto/auth-result.dto';
 import { LocalAuthGuard } from './auth/guards/local-auth.guard';
+import * as path from 'path';
 
 @Controller()
 export class AppController {
@@ -26,5 +43,17 @@ export class AppController {
   })
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @Get('/spec.json')
+  @ApiExcludeEndpoint()
+  @ApiTags('service')
+  // @ApiOperation({ description: 'Спецификация api-сервиса' })
+  async specs(@Response({ passthrough: true }) res) {
+    //
+    const file = createReadStream(
+      path.join(process.cwd(), 'swagger-spec.json'),
+    );
+    return new StreamableFile(file);
   }
 }
