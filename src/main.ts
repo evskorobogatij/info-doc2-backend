986b713b07f0688ecec822252543143664f3f06b
Скоробогатый Евгений <evskorobogatij@gmail.com>
2022-09-26 16:16:30 +0300

    Документация апи и автогенерация спецификации

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 import { AppModule } from './app.module';
+export * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Управление инфоматом')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: false,
+  });
+
+  writeFileSync('./swagger-spec.json', JSON.stringify(document));
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
