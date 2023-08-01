bfeba3f8c47eef3005853c9791a4787aca84a837
Скоробогатый Евгений <evskorobogatij@gmail.com>
2023-08-01 11:51:11 +0300

    Изменения в стратегии jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: 'process.env.JWT_KEY',
+      secret: process.env.JWT_KEY,
       signOptions: {
         expiresIn: '15m',
       },
